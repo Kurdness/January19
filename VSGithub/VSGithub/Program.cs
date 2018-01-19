68c0ddb4dc7191797d0a7a899977ca000562ac85
Student <Student@VMMB310-70>
2018-01-19 13:35:20 +0200

    Added a branch Feature 2

---
+++ VSGithub/VSGithub/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //So far you have only worked with main branch
+            //Something more
+            //Modify something
+            //Code to call Feature 1
+            //Code to call Feature 2
+
+        }
+    }
+}
